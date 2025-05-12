dc0fc4b5b3033769d81da6fc404b542c1dcc8066
himanshupapola <himanshupapola.ph@gmail.com>
2025-05-12 22:53:43 +0530

    'm'

---
+++ 13-redux/old/src/store.js
@@ -0,0 +1,28 @@
+const initalState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+// to calculate the new state based on recivied state and action
+function reducer(state = initalState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      // Later
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
